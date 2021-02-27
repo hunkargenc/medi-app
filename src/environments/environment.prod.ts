74cb2fb3a8f7209e3dbaaf6ddff9d9aa65e666b2
hunkargenchcttp <67922360+hunkargenchcttp@users.noreply.github.com>
2021-02-27 22:51:12 +0300

    firebase commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAG3FIPE-tPNhZom8lPdGYXDGf04DQFHEs",
+    authDomain: "mediapp-66a02.firebaseapp.com",
+    databaseURL: "https://mediapp-66a02-default-rtdb.firebaseio.com",
+    projectId: "mediapp-66a02",
+    storageBucket: "mediapp-66a02.appspot.com",
+    messagingSenderId: "499651276209",
+    appId: "1:499651276209:web:c41679c2daebd879cc6190"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAG3FIPE-tPNhZom8lPdGYXDGf04DQFHEs",
+    authDomain: "mediapp-66a02.firebaseapp.com",
+    databaseURL: "https://mediapp-66a02-default-rtdb.firebaseio.com",
+    projectId: "mediapp-66a02",
+    storageBucket: "mediapp-66a02.appspot.com",
+    messagingSenderId: "499651276209",
+    appId: "1:499651276209:web:c41679c2daebd879cc6190"
+  }
 };
 
 /*
